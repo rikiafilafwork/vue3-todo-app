78f7b36274961744f6badf5f298dbc17c1a79463
Riki Afila F <rikiafilaf.work@gmail.com>
2023-10-08 14:05:01 +0700

    Remove unused vars

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -49,9 +49,6 @@ function removeCompleted() {
         <input type="search"
             class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search task" @keyup.enter="searchTodos" v-model="inputSearch">
-        <button type="button"
-            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            @click="searchTodos">Search</button>
     </div>
     <div class="flex justify-end mb-3 flex-wrap" v-show="inputSearch !== ''">
         <button @click="removeSearch" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -16,8 +16,6 @@ const todos = ref(JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'))
 const newTodo = ref('')
 const visibility = ref('all')
 const editedTodo = ref()
-const classActive = 'text-blue-700 text-xs hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800'
-const classDefault = 'text-gray-900 text-xs border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800'
 
 // derived state
 const filteredTodos = computed(() => filters[visibility.value](todos.value))
